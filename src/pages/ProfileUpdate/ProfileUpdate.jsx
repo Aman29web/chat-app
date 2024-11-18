ff6623f37d53df2c70e646fe3818152902ac462a
Aman29web <amansinghvns29@gmail.com>
2024-11-18 12:00:53 +0530

    ok

---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+      <h1>Profile Update</h1>
+      
+    </div>
+  )
+}
+
+export default ProfileUpdate
